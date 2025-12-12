685c4b5c5b4dd5fb3593aa532ecad1f0233a9ecf
rahulprasadyadav-ai <rahul@techsharthi.ai>
2025-12-13 00:22:58 +0530

    added pratice

--- 2.Chat_models/1_chatmodel_openAi.py
+++ 2.Chat_models/1_chatmodel_openAi.py
@@ -8,6 +8,9 @@ load_dotenv()
 model=ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
 
 
+# temp 0  hai to same ouput aayega .. always .. 
+
+
 result=model.invoke("write 5 line for criketet")
 
 
---
+++ Prompts/Prompts.py
@@ -0,0 +1,15 @@
+
+
+
+## tempurature agar 0 rakhte hai to same output aayega
+
+
+"""
+   prompts:
+   prompts are the input instruction or queries given to model to guide its output 
+
+""" 
+
+
+
+
